3cd22a706d086dc60ae09d37d1852bbe7df972d8
T-FABIO <fabiotru04@gmail.com>
2024-03-29 01:10:15 -0500

    agregado

--- Introducción/operadores.js
+++ Introducción/operadores.js
@@ -4,15 +4,22 @@ let resultado;
 
 let n1 = parseFloat(prompt('ingresa numero 1: '));
 let n2 = parseFloat(prompt('ingresa numero 2: '));
-
+    
 function operaciones(tipo, numero1, numero2){
+
     if(tipo == 'resta' && numero1 < numero2){
+
         alert('la opreacion no se puede realizar porque ' + numero1 + ' es menor que ' + numero2);
         return;
+
     }else if(tipo == 'resta'){
+
         resultado = numero1 - numero2;
+
     }else if(tipo == 'suma'){
+
         resultado = numero1 + numero2;
+
     }
     
     confirm('resultado: ' + resultado);
