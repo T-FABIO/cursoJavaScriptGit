1a5c02db4642b490824eb8b684db4a0a751c296e
T-FABIO <fabiotru04@gmail.com>
2024-03-30 01:20:36 -0500

    operadores

--- Introducción/operadores.js
+++ Introducción/operadores.js
@@ -2,13 +2,15 @@
 
 //explicadas como una calculadora usando funciones: suma, resta, multiplicacion, division.
 
-let tipo = prompt('ingresa el tipo de operacion: ');
-let resultado;
 
-let n1 = parseFloat(prompt('ingresa numero 1: '));
-let n2 = parseFloat(prompt('ingresa numero 2: '));
     
-function operaciones(tipo, numero1, numero2){
+function operaciones(){
+
+    let tipo = prompt('ingresa el tipo de operacion: ');
+    let resultado = 0, numero1 = 0, numero2 = 0;
+    
+    numero1 = parseFloat(prompt('ingresa numero 1: '));
+    numero2 = parseFloat(prompt('ingresa numero 2: '));
 
     if(tipo == 'resta' && numero1 < numero2){
 
@@ -40,8 +42,7 @@ function operaciones(tipo, numero1, numero2){
 
 }
 
-operaciones(tipo, n1, n2);
-
+//operaciones();
 
 //Operadores de Aumento
 
@@ -51,11 +52,68 @@ numeroAumento++;
 let numeroDisminucuion = 1;
 numeroDisminucuion--;
 
-//Operadores de Asignación
+/*Operadores de Asignación
+    
+    += suma un numero al valor de una variable.
+    -= resta un numero al valor de una variable.
+    *= multiplica un numero al valor de una variable.
+    /=  divide un numero al valor de una variable.
+    %=  devuelve el residuo de un numero al valor de una variable.
+    
+*/
+
+//Ejemplo
+
+/*let numero = 5;
+numero += 5;
+console.log(numero);*/
+
+/* Operadores de Comparacion:
+    nos permiten comparar valores
+
+    == igual que en valores
+    === igual en valor y tipo de valor
+    != diferente
+    !== diferente en valor y diferente en tipo
+    > mayor que
+    < menor que
+    >= myor o igual que
+    <= menor o igual que
+    ? operador ternario
+
+*/
+
+//Ejemplo 
+
+//const resultado = 5 > 1; //true
+//const resultado = 1 > 5; //false
+//const resultado = 10 == 10; //true
+
+/*const resultado = 10 === '10'; //false 
+    en este ejemplo se usan tres simbolos iguales
+    para comparar el numero entero 10 con 
+    la cadena de texto '10'. 
+
+    los dos son el mismo numero pero no el mismo tipo. 
+*/
+
+/* Ejemplo
+const resultado1 = 10 === 10; //true
+const resultado2 = 10 === '10'; //false
+console.log('resultado 1: ' + resultado1);
+console.log('resultado 2: ' + resultado2);
+*/
+
+
+/* Operador Ternario
+    con este operador se pueden hacer comparaciones y ejecutar un codigo si se cumple la condicion
 
 
 
+*/
 
+// const resultado = 7 > 10 ? 'el primer valor es mayor que uno' : 'el primer valor es menor que uno';
+// console.log(resultado);
 
 
 
---
+++ Introducción/operadoresLogicos.js
@@ -0,0 +1,27 @@
+/*Operadores Logicos
+
+&& AND
+|| OR
+! NOT
+
+*/
+
+const nombre = 'fabio';
+const edad = 18;
+const tieneEntrada = true;
+const tienePermiso = true;
+
+// Ejemplo #1 - &&
+// const permitirAcceso = edad >= 18 && tieneEntrada;
+// console.log('Acceso permitido al concierto: ' + permitirAcceso);
+
+//Ejemplo #2 - OR
+// const permitirAcceso = (edad >= 18 && tieneEntrada) || (tienePermiso && tieneEntrada);
+// console.log('Acceso permitido al concierto: ' + permitirAcceso);
+
+//Ejemplo #3 - !
+// 📝 el simbolo (!) invierte el valor inicial )
+
+// const variable = true;
+// console.log(!variable); // false
+
